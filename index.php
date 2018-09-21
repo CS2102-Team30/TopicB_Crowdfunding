27a7d48de375269f307e8c87fc85073c68126077
andrewome <andrewong3012@gmail.com>
2018-09-21 23:25:23 +0800

    added bootstrap 4, separated registration & log in page

--- index.php
+++ index.php
@@ -1,67 +1,80 @@
 <!DOCTYPE html>  
-<head>
-    <title>UPDATE PostgreSQL data with PHP</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <style>li {list-style: none;}</style>
-</head>
+<html lang="en">
+    <head>
+        <title>Welcome to Crowdfunding!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="./css/bootstrap.css">
+        <!-- Boostrap JS dependencies -->
+        <script src="./js/bootstrap.js"></script>
+    </head>
+    
+    <body>
+        <div class="jumbotron text-center">
+            <h1>Welcome to Crowdfunding thingy</h1>
+            <p>Something something crowdfunding something</p> 
+        </div>       
+        
+        <div class="container">
+            <div class="text-center">
+                <h1>Please log in to continue</h1>
+                <br>
+            </div>
+            
+            <form name="display" action="index.php" method="POST">
+                <div class="form-group row">
+                    <label for="userid" class="col-sm-1 col-form-label">UserID: </label>
+                    <div class="col-sm-10">
+                        <input name="userid" class="form-control" placeholder="UserID" required/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="password" class="col-sm-1 col-form-label">Password: </label>
+                    <div class="col-sm-10">
+                        <input name="password" type="password" class="form-control" placeholder="Password" required/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-1">
+                        <input type="submit" name="login_submit" value="Log In"/>
+                    </div>
+                    <div class="col-sm-10 text-center">
+                        Not registered? <a href="register.php">Register here.</a>
+                    </div>
+                </div>
+            </form>
+            <br><br><br>
 
-<body>
-    <h2>Login</h2>
-    <ul>
-        <form name="display" action="index.php" method="POST" >
-            <li><input type="text" name="userid" placeholder="userid" required/></li>
-            <li><input type="password" name="password" placeholder="password" required/></li>
-            <li><input type="submit" name="login_submit" /></li>
-        </form>
-    </ul>
-    <h2>Sign up for new account</h2>
-    <ul>
-        <form name="display" action="index.php" method="POST" >
-            <li><input type="text" name="userid2" placeholder="userid" required/></li>
-            <li><input type="password" name="password2" placeholder="password" required/></li>
-            <li><input type="submit" name="signup_submit" /></li>
-        </form>
-    </ul>
+            <div class="text-center">
+                <?php
+                    session_start();
+                    // Connect to the database
+                    $db = pg_connect("host=localhost port=5432 dbname=project1 user=postgres password=test");	
+                    if(!$db) {
+                        echo "Error : Unable to open database <br>";
+                    } else {
+                        echo "Opened database successfully <br>";
+                    }
 
-    <?php
-        session_start();
-
-        // Connect to the database
-        $db = pg_connect("host=localhost port=5432 dbname=project1 user=postgres password=test");	
-        if(!$db) {
-            echo "Error : Unable to open database\n";
-        } else {
-            echo "Opened database successfully\n";
-        }
-
-        if (isset($_POST['login_submit'])) {
-            $query = "SELECT * FROM account WHERE userid='$_POST[userid]'";
-            $result = pg_query($db, $query);
-            if (!$result) {
-                echo "Login failed!";
-            }
-            else {
-                $row = pg_fetch_assoc($result);
-                if ($_POST[password] != $row[password]) {
-                    echo "Login failed!";
-                }
-                else {
-                    $_SESSION[userid] = $row[userid];
-                    header("Location: mainpage.php");
-                }
-            }
-        }
-
-        if (isset($_POST['signup_submit'])) {	
-            $query = "INSERT INTO account VALUES ('$_POST[userid2]', '$_POST[password2]')";
-            $result = pg_query($db, $query);
-            if (!$result) {
-                echo "Signup failed!";
-            }
-            else {
-                echo "Signup successful!";
-            }
-        }
-    ?>  
-</body>
+                    if (isset($_POST['login_submit'])) {
+                        $query = "SELECT * FROM account WHERE userid='$_POST[userid]'";
+                        $result = pg_query($db, $query);
+                        if (!$result) {
+                            echo "Login failed!";
+                        }
+                        else {
+                            $row = pg_fetch_assoc($result);
+                            if ($_POST[password] != $row[password]) {
+                                echo "Login failed!";
+                            }
+                            else {
+                                $_SESSION[userid] = $row[userid];
+                                header("Location: mainpage.php");
+                            }
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </body>
 </html>
---
+++ register.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>  
+<html lang="en">
+    <head>
+        <title>Register for Crowdfunding!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="./css/bootstrap.css">
+        <!-- Boostrap JS dependencies -->
+        <script src="./js/bootstrap.js"></script>
+    </head>
+    
+    <body>
+        <div class="jumbotron text-center">
+            <h1>Welcome to Crowdfunding thingy</h1>
+            <p>Something something crowdfunding something</p> 
+        </div>       
+        
+        <div class="container">
+            <div class="text-center">
+                <h1>Register here</h1>
+                <br>
+            </div>
+            
+            <form name="display" action="register.php" method="POST">
+                <div class="form-group row">
+                    <label for="reg_userid" class="col-sm-1 col-form-label">UserID: </label>
+                    <div class="col-sm-10">
+                        <input name="reg_userid" class="form-control" placeholder="UserID" required/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="reg_password" class="col-sm-1 col-form-label">Password: </label>
+                    <div class="col-sm-10">
+                        <input name="reg_password" type="password" class="form-control" placeholder="Password" required/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-1">
+                        <input type="submit" name="signup_submit" value="Sign Up!"/>
+                    </div>
+                </div>
+            </form>
+            <br><br>
+
+            <div class="text-center">
+                <?php
+                    session_start();
+                    // Connect to the database
+                    $db = pg_connect("host=localhost port=5432 dbname=project1 user=postgres password=test");	
+                    if(!$db) {
+                        echo "Error : Unable to open database\n";
+                    } else {
+                        echo "Opened database successfully\n";
+                    }
+                    
+                    if (isset($_POST['signup_submit'])) {
+                        $query = "INSERT INTO account VALUES ('$_POST[reg_userid]', '$_POST[reg_password]')";
+                        $result = pg_query($db, $query);
+                        if (!$result) {
+                            echo "Signup failed!";
+                        }
+                        else {
+                            echo "Signup successful! Return to the ";
+                            echo '<a href="index.php">main page to log in! </a>';
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
