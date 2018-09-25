5f96c80879fc2c2151b4873b94160b54804e54f4
Andrew Ong <39642465+andrewome@users.noreply.github.com>
2018-09-25 13:55:22 +0800

    modularized header file

--- index.php
+++ index.php
@@ -2,14 +2,7 @@
 <html lang="en">
     <head>
         <title>Welcome to Crowdfunding!</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="./css/styles.css">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="./css/bootstrap.css">
-        <!-- Boostrap JS dependencies -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="./js/bootstrap.js"></script>
+        <?php include("./template/head.php"); ?>
     </head>
     
     <body>      
