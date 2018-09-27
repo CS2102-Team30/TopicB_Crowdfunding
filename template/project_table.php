df9550a0a896944541cf71794955f79ee8e58c37
andrewome <andrewong3012@gmail.com>
2018-09-27 21:37:43 +0800

    changed fetch row to fetch associative array

--- template/project_table.php
+++ template/project_table.php
@@ -1,17 +1,17 @@
 <?php
 	// Getting data, index var stores the index of the rows
 	$index = 0;
-	while ($row = pg_fetch_row($result))  {
-		$projectid = $row[pg_num_fields($result)-1];
+	while ($row = pg_fetch_assoc($result))  {
+		$projectid = $row['projectid'];
 ?>
 		<div class="card" data-id="<?php echo $projectid;?>">
 			<div class="card-header">
-				<?php echo $row[0];?>
+				<?php echo $row['title'];?>
 			</div>
 			<div class="card-body">
-				<?php echo "<p>" . "Advertised by: " . $row[1] . " | " . "Currently raised: " . "$". $row[4] . "/" . "$" . $row[5] . "</p>"; ?>
+				<?php echo "<p>" . "Advertised by: " . $row['advertiser'] . " | " . "Currently raised: " . "$". $row['amount_funded'] . "/" . "$" . $row['funding_sought'] . "</p>"; ?>
 				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#projectModal" 
-				data-description="<?php echo $row[6];?>" data-title="<?php echo $row[0];?>" data-startdate="<?php echo $row[2];?>" data-duration="<?php echo $row[3];?>">
+				data-description="<?php echo $row['description'];?>" data-title="<?php echo $row['title'];?>" data-startdate="<?php echo $row['start_date'];?>" data-duration="<?php echo $row['duration'];?>">
 					Find out more
 				</button>
 			</div>
