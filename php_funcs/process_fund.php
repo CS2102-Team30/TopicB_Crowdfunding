74d5f33a18b852a4a70ed17992d56f0ab27fe04f
andrewome <andrewong3012@gmail.com>
2018-09-28 00:30:12 +0800

    added sql queries

--- php_funcs/process_fund.php
+++ php_funcs/process_fund.php
@@ -5,17 +5,14 @@
     
     $add = $_POST['amount'];
     $projectid = $_POST['projectid'];
+    $userid = $_SESSION['userid'];
     
     $query = "UPDATE projects SET amount_funded = amount_funded + $add WHERE projectid = '$projectid'";
     
-    $result = pg_query($db, $query);                  
-    if (!$result) {
-        echo "Project submission failed, please try again";
-		$_SESSION['submit_state'] = "failed";
-    }
-    else {
-        echo "Project successfully submitted";
-		$_SESSION['submit_state'] = "success";
-	}
-    header("Location: ../main.php");
+    $result = pg_query($db, $query);
+
+    $query = "INSERT INTO invest VALUES ('$userid', '$projectid', $add)";
+    $result = pg_query($db, $query);
+
+    //header("Location: ../main.php");
 ?>
