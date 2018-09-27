b966ba04803ad70df978f0e89675dab1aea5d7d1
andrewome <andrewong3012@gmail.com>
2018-09-27 20:38:21 +0800

    fixed more warnings

--- php_funcs/checkLogIn.php
+++ php_funcs/checkLogIn.php
@@ -2,7 +2,7 @@
     session_start(); 
     
     //if user is logged in, redirect to main.php
-    if(isset($_SESSION[userid])) {
+    if(isset($_SESSION['userid'])) {
         header("Location: main.php");
     }
 ?>
