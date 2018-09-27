89e53e8c39b5f5127c4beb31305f2b4cb7286445
andrewome <andrewong3012@gmail.com>
2018-09-27 18:30:25 +0800

    Revert "Merge branch 'JQuery-DataTables'"
    This reverts commit e8b3858e19824cdb52bed70ce83a04968779daca, reversing
    changes made to 8471fb19020ba0ff38192deb78253b40b5bd7697.

--- main.php
+++ main.php
@@ -23,8 +23,9 @@
 			<ul class="nav nav-tabs">
 				<li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#viewAll">View All</a></li>
 				<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#viewFunded">View Funded</a></li>
+				<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#search">Search</a></li>
 			</ul>
-            
+			
 			<!-- Tab content -->
 			<div class="tab-content">
 				<div id="viewAll" class="tab-pane show active">
@@ -34,10 +35,7 @@
 					<br>
 					<?php
 						// Retrieving projects from DB
-						$result = pg_query('SELECT title, advertiser, start_date, duration, amount_funded, funding_sought, projectid, description FROM projects');
-                        
-                        // Converting to JSON object for DataTables to utilise
-                        $resultArray = pg_fetch_all($result);
+						$result = pg_query('SELECT title, advertiser, start_date, duration, amount_funded, funding_sought, description, projectid FROM projects');
 					?>
 
 					<?php include('./template/project_table.php'); ?>
@@ -57,84 +55,79 @@
 
 					<?php include('./template/project_table.php'); ?>
 				</div>
+				
+				<div id="search" class="tab-pane fade">
+					<br>
+					<h3>Can't find what you're looking for?</h3>
+					<p> Don't worry, we got you covered.</p>
+					<form action="main.php" method="POST">
+						<label for="search_field" class="col-lg-1 col-form-label">Search: </label>
+						<input name="search_field" class="form-control" placeholder="Any relevant keywords" required/>
+						<br>
+						<div class="text-center">
+							<button class="btn btn-primary" type="submit" name="search">Search</button>
+						</div>
+					</form>
+					<br> 
+					<?php
+						if (isset($_POST['search'])) {
+							//Searches title and keywords, Finds any values that have "word" in any position, Case-insensitive
+							$result = pg_query("SELECT title, advertiser, start_date, duration, amount_funded, funding_sought, description, projectid FROM projects
+								WHERE UPPER(title) LIKE UPPER('%$_POST[search_field]%')
+								OR UPPER(keywords) LIKE UPPER('%$_POST[search_field]%')");
+						}
+					?>
+					
+					<?php include('./template/project_table.php'); ?>
+				</div>
 			</div>
+			
+            <?php
+                if (isset($_POST[logout_submit])) {
+                    include('./php_funcs/logOut.php');
+                }
+            ?>
         </div>
         
-        <!-- DataTable JS Script here -->
         <script>
-            function format ( d ) {
-                // `d` is the original data object for the row
-                console.log(d.start_date);
-                var startDate = new Date(d.start_date);
-                var endDate = new Date(d.start_date);
-                endDate.setTime(endDate.getTime() + d.duration*86400000);
+            $(document).ready(function () {
+                $("#projectModal").on('show.bs.modal', function (event) {
+					var button = $(event.relatedTarget);
+					var modal = $(this);
+					modal.find('.modal-title').text("Details about " + button.data('title'));
+					modal.find('.modal-body #description').text("Description: " + button.data('description'));
+					modal.find('.modal-body #startdate').text("Start Date: " + button.data('startdate'));
+					modal.find('.modal-body #duration').text("Duration: " + button.data('duration') + " days");
+                });
+
+			// 	console.log($(this).data());
                 
-                return '<table>'+
-                    '<tr>'+
-                        '<td>Description:</td>'+
-                        '<td>'+d.description+'</td>'+
-                    '</tr>'+
-                    '<tr>'+
-                        '<td>Duration of project:</td>'+
-                        '<td>'+startDate.toLocaleDateString("en-UK")+' to '+endDate.toLocaleDateString("en-UK")+'</td>'+
-                    '</tr>'+
-                    '<tr>'+
-                        '<td>Progress:</td>'+
-                        '<td>'+'$' + d.amount_funded + ' raised/' + '$' + d.funding_sought+' required'+'</td>'+
-                    '</tr>'+
-                    '<tr>'+
-                        '<td>Project ID:</td>'+
-                        '<td>'+d.projectid+'</td>'+
-                    '</tr>'+
-                '</table>';
-            }
-            
-            var resultdata = <?php echo json_encode($resultArray); ?>;
-            
-            $(document).ready(function() {
-                var table = $('#projectTable').DataTable({
-                    "data": resultdata,
-                    "columnDefs": [
-                        { 
-                            "className": "details-control",
-                            "targets": "_all"
-                        },
-                        {
-                            "targets": [3],
-                            "visible": false
-                        },
-                        {
-                            "targets": [4],
-                            "visible": false
-                        }
-                    ],
-                    "columns": [
-                        { "data": "title" },
-                        { "data": "advertiser" },
-                        { "data": "amount_funded" },
-                        { "data": "description" },
-                        { "data": "projectid" },
-                    ],
-                    "order": [[2, 'asc']]
-                });
-            
-                // Add event listener for opening and closing details
-                $('#projectTable tbody').on('click', 'td.details-control', function () {
-                    var tr = $(this).closest('tr');
-                    var row = table.row( tr );
-             
-                    if ( row.child.isShown() ) {
-                        // This row is already open - close it
-                        row.child.hide();
-                        tr.removeClass('shown');
-                    }
-                    else {
-                        // Open this row
-                        row.child( format(row.data()) ).show();
-                        tr.addClass('shown');
-                    }
-                });
+            //     content += $(this).data().id;
+            //      $('.modal-body').load(content, function() {
+            //         $('#projectModal').modal();
+            //     });
             });
         </script>
     </body>
+    
+    <!-- Modal -->
+    <div id="projectModal" class="modal fade" role="dialog" tabindex="-1" aria-labelledby="projectModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" style="text-left"></h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+					<p id="description"/>
+					<p id="startdate"/>
+					<p id="duration"/>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>  
+        </div>
+    </div>
 </html>
